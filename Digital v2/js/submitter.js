106d036ec43d6da7a1ac4966ac9396fc162fe8cc
Eric Miller <eric@legoaces.org>
2015-03-21 00:31:17 -0400

    Fixed indentation and added file

---
+++ Digital v2/js/submitter.js
@@ -0,0 +1,12 @@
+/*
+
+This file handles submitting the forms for Scouting data, including saving in localStorage if needed.
+
+
+@Author Eric Miller<eric@legoaces.org>
+*/
+
+function toCodedString (form){
+
+	alert('hello!');
+}
