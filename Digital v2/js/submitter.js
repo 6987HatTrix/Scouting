631a58d1d8df936e35f098273b5561fcc9612dff
Eric Miller <eric@legoaces.org>
2015-03-21 01:37:42 -0400

    Interface pretty much done (one team)

--- Digital v2/js/submitter.js
+++ Digital v2/js/submitter.js
@@ -5,8 +5,34 @@ This file handles submitting the forms for Scouting data, including saving in lo
 
 @Author Eric Miller<eric@legoaces.org>
 */
-
+"use strict"
 function toCodedString (form){
+	event.preventDefault();
+	var elements = form.elements
+	var str = []
+	for (var i = 0; i < elements.length; i++){
+		var e = elements[i]
+		if (!e.id) continue;
+		str.push(e.id + ":" + e.value)
+		//console.log("id: %s, value: %s", e.id, e.value)
 
-	alert('hello!');
+	}
+	return str.join(',')
 }
+
+function saveForm(){
+	var form = event.target;
+	var str = toCodedString(form)
+	console.log(str)
+
+}
+
+window.setInterval(function(){
+	if(window.navigator.onLine){
+		$('.submitButton').text('Submit!');
+	}else{
+		$('.submitButton').text("Save Offline");
+	}
+	$('.submitButton').button('refresh')
+
+}, 200)
